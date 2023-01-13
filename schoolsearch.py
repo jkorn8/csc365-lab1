cfd5b5d36eeb38105fd18c99c696e18314077fb9
Dev Masrani <devmasrani@Devs-MacBook-Pro.local>
2023-01-13 11:02:39 -0800

    updated schoolsearch

--- schoolsearch.py
+++ schoolsearch.py
@@ -16,11 +16,34 @@ def parse_data():
 def filter_data():
     pass
 
+def filter_student():
+    pass
+def filter_teacher():
+    pass
+def filter_bus():
+    pass
+def filter_grade(High,num):
+    pass
+def filter_avg():
+    pass
 
 if __name__ == '__main__':
-    students_data = parse_data()
+    students_data = parse_data()    
     while True:
         user_input = input("Enter a command: ")
         arr = user_input.split(' ')
+        print(arr)
+        if(arr[0][0] == "S"):
+            filter_student()
+        elif(arr[0][0] == "T"):
+            filter_teacher()
+        elif(arr[0][0] == "B"):
+            filter_bus()
+        elif(arr[0][0] == "G"):
+            filter_grade()
+        elif(arr[0][0] == "A"):
+            filter_avg()
+        elif(arr[0][0] == "I"):
+            print("S[tudent]: <lastname> [B[us]]\nT[eacher]: <lastname>\nB[us]: <number>\nG[rade]: <number> [H[igh]|L[ow]]\nA[verage]: <number>\nI[nfo]\nQ[uit]\n")
         if arr[0][0] == 'Q':
             break
